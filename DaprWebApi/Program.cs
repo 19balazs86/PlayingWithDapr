5107e3989bb74d32cab4121657a97aaf4f44a8e5
Balazs Kereszturi <19balazs86@gmail.com>
2024-08-10 10:58:53 +0200

    Add new project DaprWebApi

---
+++ DaprWebApi/Program.cs
@@ -0,0 +1,27 @@
+namespace DaprWebApi;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        IServiceCollection services = builder.Services;
+
+        // Add services to the container
+        {
+
+        }
+
+        WebApplication app = builder.Build();
+
+        // Configure the HTTP request pipeline
+        {
+            app.UseHttpsRedirection();
+
+            app.MapGet("/", () => "Hello DaprWebApi");
+        }
+
+        app.Run();
+    }
+}
