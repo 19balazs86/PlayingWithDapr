7e0232f6be86c678bf628d35a789ffa993d9ebe7
Balazs Kereszturi <19balazs86@gmail.com>
2024-08-19 11:47:12 +0200

    Add missing link in a comment

--- bicep-script/UpdateDaprApiImage.ps1
+++ bicep-script/UpdateDaprApiImage.ps1
@@ -16,8 +16,11 @@ az containerapp update `
 # It can make additional changes, see to the documentation
 # https://learn.microsoft.com/en-us/cli/azure/containerapp?view=azure-cli-latest#az-containerapp-update
 
+# -------------------------------------------
 # Create a new revision based on the previous
 # It can be used as the 'az containerapp update'
+# https://learn.microsoft.com/en-us/cli/azure/containerapp/revision?view=azure-cli-latest#az-containerapp-revision-copy
+
 # az containerapp revision copy `
 #     --name "dapr-api" `
 #     --resource-group "ContainerAppDaprTest" `
