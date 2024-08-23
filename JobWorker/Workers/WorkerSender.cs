5cfd2fd1b42d7a3eeffd69e11d09fb6e6c519514
Balazs Kereszturi <19balazs86@gmail.com>
2024-08-23 11:39:07 +0200

    Adopt WorkerSender to the WorkerBase class

--- JobWorker/Workers/WorkerSender.cs
+++ JobWorker/Workers/WorkerSender.cs
@@ -4,67 +4,32 @@ using Microsoft.Extensions.Options;
 
 namespace JobWorker.Workers;
 
-public sealed class WorkerSender : BackgroundService
+public sealed class WorkerSender : WorkerBase
 {
-    private readonly ILogger<WorkerSender> _logger;
-
-    private readonly QueueClient _queueClient;
-
-    private readonly WorkerSettings _queueSettings;
-
-    private readonly IHostApplicationLifetime _hostApplicationLifetime;
-
     public WorkerSender(
         ILogger<WorkerSender> logger,
         QueueServiceClient queueServiceClient,
-        IOptions<WorkerSettings> queueOptions,
-        IHostApplicationLifetime hostApplicationLifetime)
+        IOptions<WorkerSettings> workerOptions,
+        IHostApplicationLifetime hostApplicationLifetime) : base(logger, queueServiceClient, workerOptions.Value, hostApplicationLifetime)
     {
-        _logger = logger;
 
-        _queueSettings = queueOptions.Value;
-
-        _queueClient = queueServiceClient.GetQueueClient(_queueSettings.JobQueueName);
-
-        _hostApplicationLifetime = hostApplicationLifetime;
     }
 
-    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    protected override async Task doWorkAsync(CancellationToken stoppingToken)
     {
-        try
+        _logger.LogInformation("Sending {number} messages", _workerSettings.SendNumberOfMessages);
+
+        JobMessage[] jobMessages = JobMessage.CreateMore(_workerSettings.SendNumberOfMessages).ToArray();
+
+        for (int i = 0; i < _workerSettings.SendNumberOfMessages && !stoppingToken.IsCancellationRequested; i++)
         {
-            await _queueClient.CreateIfNotExistsAsync();
+            BinaryData message = BinaryData.FromObjectAsJson(jobMessages[i]);
 
-            do
-            {
-                _logger.LogInformation("Sending {number} messages", _queueSettings.SendNumberOfMessages);
+            await _queueClient.SendMessageAsync(message);
 
-                JobMessage[] jobMessages = JobMessage.CreateMore(_queueSettings.SendNumberOfMessages).ToArray();
-
-                for (int i = 0; i < _queueSettings.SendNumberOfMessages && !stoppingToken.IsCancellationRequested; i++)
-                {
-                    BinaryData message = BinaryData.FromObjectAsJson(jobMessages[i]);
-
-                    await _queueClient.SendMessageAsync(message);
-
-                    await Task.Delay(500);
-                }
-
-                _logger.LogInformation("Messages are sent");
-
-                if (_queueSettings.IsLongRunningApp)
-                {
-                    await Task.WhenAny(Task.Delay(5_000, stoppingToken));
-                }
-            } while (_queueSettings.IsLongRunningApp && !stoppingToken.IsCancellationRequested);
+            await Task.Delay(500);
         }
-        catch (Exception ex)
-        {
-            _logger.LogError(ex, "Failed to send messages");
-        }
-        finally
-        {
-            _hostApplicationLifetime.StopApplication();
-        }
+
+        _logger.LogInformation("Messages are sent");
     }
 }
