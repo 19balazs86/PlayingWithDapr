ecb42160fe64c2ae9a526a95863c9015c35f9e4e
Balazs Kereszturi <19balazs86@gmail.com>
2024-08-30 12:23:52 +0200

    Write a comment about incoming binding checking APP_API_TOKEN

--- DaprWebApi/Endpoints/OrderBindingEndpoints.cs
+++ DaprWebApi/Endpoints/OrderBindingEndpoints.cs
@@ -27,6 +27,17 @@ public sealed class OrderBindingEndpoints : IEndpoint
 
     private static StatusCodeHttpResult checkout(Order order, ILogger<OrderBindingEndpoints> logger)
     {
+        /* To ENSURE the incoming request is initiated by the Dapr sidecar, you can check
+         * - Request.Headers["dapr-api-token"] == Environment.GetEnvironmentVariable("APP_API_TOKEN")
+         *
+         * Setup in Self-Hosted mode
+         * - You need to set the APP_API_TOKEN environment variable
+         * - Dapr sidecar will include this token in all requests sent to your app
+         * - Documentation: https://docs.dapr.io/operations/security/app-api-token
+         *
+         * - Azure Container App: https://learn.microsoft.com/en-us/azure/container-apps/dapr-authentication-token
+         */
+
         // Simulate server error. Dapr will resend the storage queue message with visibilityTimeout
         int statusCode = Random.Shared.NextDouble() <= 0.8 ? StatusCodes.Status200OK : StatusCodes.Status500InternalServerError;
 
